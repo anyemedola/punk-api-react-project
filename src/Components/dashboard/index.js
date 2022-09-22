62b7b5ebf694ade2400a3fdb86939579f20b0669
Any Medola <anynhamedola@gmail.com>
2022-09-22 14:33:19 -0300

    dash

--- src/Components/dashboard/index.js
+++ src/Components/dashboard/index.js
@@ -1,9 +1,16 @@
+import Sidebar from './sidebar';
+import Main from './main';
 import './style.css'
 
 export function Dashboard() {
 
     return (
-        <></>
+        <div className="container">
+            <div className="dashboard">
+                <Sidebar />
+                <Main />
+            </div>
+        </div>
     )
 
 }
